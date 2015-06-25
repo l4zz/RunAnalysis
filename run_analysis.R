31ff2a7966349b99b890fdbd6f00a0a828af53ff
Luis Santos <l.santos@har.mrc.ac.uk>
2015-06-25 01:08:54 +0100

    calculating means of all variables, must improve the variable names

--- run_analysis.R
+++ run_analysis.R
@@ -42,4 +42,36 @@ for (i in seq_len(nrow(activityLabels))) {
 	singleSet$activity[singleSet$activity %in% activityLabels[i,1]] <- as.character(activityLabels[i,2])
 }
 
-	
+# just a quick name change
+oldnames <- names(singleSet)
+oldnames <- gsub('.', '', oldnames, fixed=T)
+#oldnames <- gsub('mean', "_mean_", oldnames, fixed=T)
+#oldnames <- gsub('std', "_std_", oldnames, fixed=T)
+names(singleSet) <- tolower(oldnames)
+# names of variables should be:
+# - all lower case when possible
+# - descriptive (diagnosis vs Dx)
+# - not duplicated
+# - not have underscores or dots or white spaces
+
+# acc - accelerometer; split up into:
+#  BodyAcc - bodyacceleration
+#  GravityAcc - gravityacceleration
+# gyr - gyroscope
+# t - time
+# f - fastfouriertransform
+# XYZ - XYZ axis
+
+# calculating the avgs for each variable
+tidyData <- aggregate(singleSet, by=list(singleSet$activity, singleSet$subject), FUN=mean)
+drop <- c("activity", "subject")
+tidyData <- tidyData[, !(names(tidyData) %in% drop)]
+oldnames <- names(tidyData)
+oldnames <- gsub('Group.1', 'activity', oldnames, fixed=T)
+oldnames <- gsub('Group.2', 'subject', oldnames, fixed=T)
+oldnames <- gsub('bodybody', 'body', oldnames, fixed=T)
+#oldnames <- gsub('', 'mean_.Body', oldnames)
+names(tidyData) <- oldnames
+
+#write.table(tidyData, file="UCI_HAR_tidy.tsv", sep="\t", row.names=F)
+
